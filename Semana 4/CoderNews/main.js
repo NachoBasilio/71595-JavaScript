73234c025c075aba952eeea671f101da8a79a956
IgnaDev <arcortoon@gmail.com>
2024-07-11 21:31:09 -0300

    Semana 4 agregada

---
+++ Semana 4/CoderNews/main.js
@@ -0,0 +1,38 @@
+// Organización de noticias en "coderNews":
+// El equipo de "coderNews" ha desarrollado un sistema basado en arrays para gestionar y organizar noticias en su plataforma.
+
+// El editor jefe de "coderNews" anuncia que el equipo ha comenzado agregando la primera noticia al array, titulada "Nueva actualización del sistema operativo".
+
+
+// Agregando más noticias:
+// El artículo informa cómo el equipo agrega noticias adicionales a su lista utilizando el método "push" o "unshift".
+
+
+// Eliminando noticias:
+// El equipo está revisando qué noticias son más relevantes. La página de noticias revela cómo eliminan la primera noticia utilizando el método "shift".
+
+// noticias.shift()
+
+// También se informa que el equipo decide eliminar la última noticia utilizando el método "pop".
+
+// noticias.pop()
+
+//  Actualizando noticias:
+// El equipo decide actualizar una noticia existente utilizando el método "splice". El artículo detalla cómo modifican una noticia existente y agregan una nueva en su lugar.
+
+// noticias[1] = "Nueva actualización del sistema operativo en PC"
+
+
+//  Uniendo las noticias:
+// El artículo muestra cómo los periodistas utilizan el método "join" para unir todas las noticias en una sola cadena legible.
+
+
+//  El editor en jefe revela que el equipo está revisando el contenido utilizando los métodos "indexOf" e "includes" para identificar ciertas noticias.
+
+
+// Longitud del array de noticias:
+//  El editor destaca el uso del método "length" para obtener el número total de noticias en la lista.
+
+
+//  Obtener una copia de las primeras dos noticias utilizando el método "slice":
+
---
+++ Semana 4/Ejercicios/ejercicios.js
@@ -0,0 +1,34 @@
+// Arrays
+
+/*
+
+1. Escribir un array de strings con una longitud de 4.
+2. Escribir un array con 6 números.
+3. Con el array del punto 2, recorrer el array y dar la sumatoria de esos 6 números.
+4. Realizar una función que solicite al usuario que ingrese sus 3 comidas favoritas. Con esa información, crear un array de comidas.
+5. Pedirle al usuario que ingrese las tres primeras letras de su país. Comparar con el siguiente array:
+
+const paises = ["ARG", "BRA", "URU", "MEX", "COL"]
+
+Si el país está dentro de estos, mostrar un mensaje de alerta que mencione que el país está en nuestra "base de datos". Caso contrario, agregar el nuevo país a esta "base de datos" y mostrar un mensaje de alerta avisándole al usuario que se ha agregado un nuevo país. OJO: las validaciones son caseSensitive así que se recomienda utilizar un .toUpperCase() para evitar problemas.
+
+
+*/
+
+
+// Objetos
+
+/*
+
+1. Crear un objeto literal donde presentes tus datos: nombre, apellido, edad y hobby.
+2. A partir del objeto del punto 1, mostrar un mensaje de alerta con el siguiente formato:
+
+"Hola, mi nombre es NOMBRE y mi apellido es APELLIDO. Tengo EDAD años de edad y mi hobby es HOBBY"
+
+(Reemplazar las mayúsculas por variables).
+
+3. Crear una clase constructora que reciba los datos del punto 1 para poder hacer nuevos objetos.
+
+4. FINAL BOSS: Crear una función que pregunte al usuario si quiere ingresar una nueva persona. En caso de responder que sí, solicitar los datos necesarios para utilizar la clase del punto 3. Una vez creado ese nuevo objeto, agregarlo a un array de objetos. Repetir este ciclo las veces que el usuario siga diciendo que sí. En caso que el usuario diga que no, mostrar la lista de personas creadas y un mensaje de despedida.
+
+*/
